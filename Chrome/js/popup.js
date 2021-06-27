6b860774b65df74dd17c63cf8b51f4b8aa894867
edevars <e.devars.dev@gmail.com>
2021-06-27 13:11:35 -0500

    :bug: Fixing layout for new styles

--- Chrome/js/popup.js
+++ Chrome/js/popup.js
@@ -1,84 +1,91 @@
 let switchButton = document.getElementById("switch");
-let slider = document.querySelector('.slider')
+let slider = document.querySelector(".slider");
 
 chrome.storage.sync.get("state", ({ state }) => {
-    if (state) {
-        switchButton.classList.add('activeSwitch')
-        slider.classList.add('activeSlider')
-    } else {
-        switchButton.classList.remove('activeSwitch')
-        slider.classList.remove('activeSlider')
-    }
+  if (state) {
+    switchButton.classList.add("activeSwitch");
+    slider.classList.add("activeSlider");
+  } else {
+    switchButton.classList.remove("activeSwitch");
+    slider.classList.remove("activeSlider");
+  }
 });
 
 switchButton.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-    chrome.storage.sync.get("state", ({ state }) => {
-        if (state) {
-            switchButton.classList.remove('activeSwitch')
-            slider.classList.remove('activeSlider')
-            chrome.scripting.executeScript({
-                target: { tabId: tab.id },
-                function: removeLayout,
-            });
-        } else {
-            switchButton.classList.add('activeSwitch')
-            slider.classList.add('activeSlider')
-            chrome.scripting.executeScript({
-                target: { tabId: tab.id },
-                function: setLayout,
-            });
-        }
-        let newState = !state;
-        chrome.storage.sync.set({ state: newState });
-    });
-}
-)
+  chrome.storage.sync.get("state", ({ state }) => {
+    if (state) {
+      switchButton.classList.remove("activeSwitch");
+      slider.classList.remove("activeSlider");
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: removeLayout,
+      });
+    } else {
+      switchButton.classList.add("activeSwitch");
+      slider.classList.add("activeSlider");
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: setLayout,
+      });
+    }
+    let newState = !state;
+    chrome.storage.sync.set({ state: newState });
+  });
+});
 
 function setLayout() {
-    let container = document.querySelector('.MaterialView');
-    container.style.height = "auto"
-    container.style.gridTemplateAreas = '"video" "content" "community"'
-    container.style.gridTemplateColumns = '1fr'
-    container.style.maxHeight = "none"
-    container.style.maxWidth = "100%"
+  let container = document.querySelector(".MaterialView");
+  container.style.display = "grid";
+  container.style.height = "auto";
+  container.style.gridTemplateAreas = '"video" "content" "community"';
+  container.style.gridTemplateColumns = "1fr";
+  container.style.maxHeight = "none";
+  container.style.maxWidth = "1920px";
 
-    let videoWrapper = document.querySelector('.MaterialView-video')
-    videoWrapper.style.maxHeight = 'none';
-    videoWrapper.style.height = 'auto';
-    videoWrapper.style.overflowY = 'inherit';
+  let videoWrapper = document.querySelector(".MaterialView-video");
+  videoWrapper.style.maxHeight = "none";
+  videoWrapper.style.height = "auto";
+  videoWrapper.style.overflowY = "inherit";
 
-    let community = document.querySelector('.MaterialView-community')
-    community.style.maxHeight = 'none';
-    community.style.height = 'auto';
-    community.style.overflowY = 'inherit';
+  let community = document.querySelector(".MaterialView-community");
+  community.style.maxHeight = "none";
+  community.style.height = "auto";
+  community.style.overflowY = "inherit";
+  community.style.width = "70%";
+  community.style.margin = "0 auto";
+  community.style.maxWidth = "none";
 
-    let communityWrapper = document.querySelector('.MaterialView-community-wrapper');
-    communityWrapper.style.maxHeight = 'none';
-    communityWrapper.style.height = 'auto';
-    communityWrapper.style.overflowY = 'inherit';
+  let communityWrapper = document.querySelector(
+    ".MaterialView-community-wrapper"
+  );
+  communityWrapper.style.maxHeight = "none";
+  communityWrapper.style.height = "auto";
+  communityWrapper.style.overflowY = "inherit";
 }
 
 function removeLayout() {
-    let container = document.querySelector('.MaterialView');
-    container.style.gridTemplateAreas = '"video community" "content community"'
-    container.style.gridTemplateColumns = '65% 35%'
-    container.style.maxHeight = "calc(100vh - 52px)"
-    container.style.maxWidth = "1440px"
+  let container = document.querySelector(".MaterialView");
+  container.style.gridTemplateAreas = '"video community" "content community"';
+  container.style.gridTemplateColumns = "65% 35%";
+  container.style.maxHeight = "calc(100vh - 52px)";
+  container.style.maxWidth = "1440px";
 
-    let videoWrapper = document.querySelector('.MaterialView-video')
-    videoWrapper.style.maxHeight = 'calc(100vh - 52px)';
-    videoWrapper.style.height = 'inherit';
-    videoWrapper.style.overflowY = 'scroll';
+  let videoWrapper = document.querySelector(".MaterialView-video");
+  videoWrapper.style.maxHeight = "calc(100vh - 52px)";
+  videoWrapper.style.height = "inherit";
+  videoWrapper.style.overflowY = "scroll";
 
-    let community = document.querySelector('.MaterialView-community')
-    community.style.maxHeight = 'calc(100vh - 52px)';
-    community.style.height = 'inherit';
-    community.style.overflowY = 'visible';
+  let community = document.querySelector(".MaterialView-community");
+  community.style.maxHeight = "calc(100vh - 52px)";
+  community.style.height = "inherit";
+  community.style.overflowY = "visible";
 
-    let communityWrapper = document.querySelector('.MaterialView-community-wrapper');
-    communityWrapper.style.maxHeight = 'calc(100vh - 52px)';
-    communityWrapper.style.height = 'inherit';
-    communityWrapper.style.overflowY = 'scroll';
+  let communityWrapper = document.querySelector(
+    ".MaterialView-community-wrapper"
+  );
+  communityWrapper.style.maxHeight = "calc(100vh - 52px)";
+  communityWrapper.style.height = "inherit";
+  communityWrapper.style.overflowY = "scroll";
 }
